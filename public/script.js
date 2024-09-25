8b4a0ddff2cb9853e69576617d9841eb0f8d24b2
nasserAF <nasser_af73@hotmail.com>
2024-09-25 15:17:12 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     // Load Navigation Bar
-    fetch('/navigationbar.htm')
+    fetch('/rahaschool/public/navigationbar.htm')
         .then(response => response.text())
         .then(data => {
             const navbarPlaceholder = document.getElementById('navbar-placeholder');
@@ -19,46 +19,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // Initialize Page
     initializePage();
 });
-
-/* document.addEventListener("DOMContentLoaded", function() {
-    // Function to load the navigation bar
-    function loadNavbar(url) {
-        return fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
-                }
-                return response.text();
-            })
-            .then(data => {
-                const navbarPlaceholder = document.getElementById('navbar-placeholder');
-                if (navbarPlaceholder) {
-                    navbarPlaceholder.innerHTML = data;
-                    initializeNavbar();
-                } else {
-                    console.error('عنصر navbar-placeholder غير موجود في HTML.');
-                }
-            });
-    }
-
-    // Attempt to load the primary navigation bar
-    loadNavbar('navigationbar.htm')
-        .catch(error => {
-            console.error('خطأ في تحميل navigationbar.htm:', error);
-            console.log('محاولة تحميل demo/navigationbar.htm بدلاً من ذلك.');
-            // Attempt to load the fallback navigation bar
-            return loadNavbar('../navigationbar.htm');
-        })
-        .catch(error => {
-            console.error('خطأ في تحميل demo/navigationbar.htm:', error);
-            // Optionally, you can handle further fallback or notify the user here
-        })
-        .finally(() => {
-            // Initialize the rest of the page regardless of navbar loading outcome
-            initializePage();
-        });
-}); */
-
 
 function initializeNavbar() {
     // Toggle Mobile Menu
